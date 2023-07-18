c2c9a9ac68ff88f3ea156aeaa5ca2ae6764fab32
Igor <iroztropinski@gmail.com>
2023-07-18 20:50:45 +0200

    Improving Records.copy

--- src/experiments.ts
+++ src/experiments.ts
@@ -2,24 +2,34 @@ import * as Records from "./records.js";
 import crypto from 'crypto';
 
 type UUID = string;
+type Timestamp = number;
 
 class User {
-    constructor(readonly id: UUID,
+    constructor(
+        readonly id: UUID,
         readonly name: string,
         readonly email: string,
-        readonly createdAt: number,
-        readonly roles: UserRole[]) {
+        readonly createdAt: Timestamp,
+        readonly roles: UserRole[]) { }
+
+    firstRole(): UserRole | null {
+        return this.roles ? this.roles[0] : null;
     }
 }
 
 class UserRole {
-    constructor(readonly name: string, readonly description: string) {
-    }
+    constructor(readonly name: string, readonly description: string) { }
 }
 
-const user = new User(crypto.randomUUID(), "Igor", "igor@gmail.com", Date.now(), 
+const user = new User(crypto.randomUUID(), "Igor", "igor@gmail.com", Date.now(),
     [new UserRole("Admin", "some admin")]);
 
+const changedUser1 = Records.copy(user, { createdAt: Date.now() + 1000, email: "gmail@igor.com" });
+const changedUser2 = Records.copy(user, { roles: [] });
+
 console.log("Source user: ", user);
-console.log("Changed user 1:", Records.copy(user, { createdAt: Date.now() - 1000}));
-console.log("Changed user 2:", Records.copy(user, { roles: []}));
+console.log("Changed user 1:", changedUser1);
+console.log("Changed user 2:", changedUser2);
+console.log("Type of source user:", user.constructor);
+console.log("Type of user 1:", changedUser1.constructor);
+console.log("First role of user 1:", changedUser1.firstRole());
--- src/records.ts
+++ src/records.ts
@@ -1,4 +1,31 @@
-export function copy<T>(source: T, toOverride: any): T {
-    //TODO: check types and keys
-    return {...source, ...toOverride};
+export function copy<T>(source: T, toReplaceFields: any): T {
+    verifyToReplaceFieldsExist(source, toReplaceFields);
+
+    const result = { ...source, ...toReplaceFields };
+
+    Object.setPrototypeOf(result, source as any);
+
+    return result;
+}
+
+function verifyToReplaceFieldsExist(source: any, toReplaceFields: any) {
+    const sourceType = source.constructor.name;
+
+    for (const k in toReplaceFields) {
+        if (!(k in source)) {
+            throw new RecordsCopyError(`${k} property is not present in the ${sourceType} record`);
+        }
+
+        const requiredType = source[k].constructor.name;
+        const givenType = toReplaceFields[k].constructor.name;
+        if (requiredType != givenType) {
+            throw new RecordsCopyError(`${sourceType} ${k} field is of ${requiredType} type, but ${givenType} was given`);
+        }
+    }
+}
+
+export class RecordsCopyError extends Error {
+    constructor(readonly message: string) {
+        super(message);
+    }
 }
